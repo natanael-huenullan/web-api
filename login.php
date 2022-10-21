99350e00de428e12783927389852967d75833390
natanael-huenullan <na.huenullan@duocuc.cl>
2022-10-21 19:52:55 -0300

    123

--- login.php
+++ login.php
@@ -8,8 +8,13 @@
         $query = mysqli_query($con, "SELECT * FROM usuarios WHERE nombre = '$nombre' AND telefono = '$telefono'");
         if($query->num_rows > 0){
             $res=array('status' => 200);
-        }else{
+        }
+        else if(!$query){
             $res=array('status' => 500,'mess'=>'usuario o password incorrecto');
+        }
+        
+        else{
+            $res=array('status' => 404,'mess'=>'error interno');
         }
         echo json_encode($res);
     }
