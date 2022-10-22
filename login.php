7135329de4f051260dedcc24a9ee80b28433fb78
natanael-huenullan <na.huenullan@duocuc.cl>
2022-10-21 22:00:57 -0300

    v145

--- login.php
+++ login.php
@@ -3,9 +3,9 @@
     if ($_SERVER['REQUEST_METHOD'] === 'POST'){
         $json = file_get_contents('php://input');
         $data = json_decode($json, true);
-        $correo = $data['correo'];
+        $correo = $data['email'];
         $contrasena =$data['contrasena'];
-        $query = mysqli_query($con, "SELECT * FROM usuarios WHERE correo = '$correo' AND contrasena = '$contrasena'");
+        $query = mysqli_query($con, "SELECT * FROM usuarios WHERE email = '$correo' AND contrasena = '$contrasena'");
         if($query->num_rows > 0){
             $res=array('status' => 200);
         }else{
