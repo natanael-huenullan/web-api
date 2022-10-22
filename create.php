5fa8257e5fef3f522a20f5990a5ac080fc3369a6
natanael-huenullan <na.huenullan@duocuc.cl>
2022-10-21 21:41:23 -0300

    v12

--- create.php
+++ create.php
@@ -6,8 +6,12 @@
     $nombre = $data['nombre'];
     $telefono = $data['telefono'];
     $email = $data['email'];
+    $APaterno = $data['ApellidoP'];
+    $AMaterno = $data['ApellidoM'];
+    $pass = $data['contrasena'];
 
-    $q = mysqli_query($con, "INSERT INTO usuarios (nombre, telefono, email) VALUES ('$nombre', '$telefono', '$email')");
+
+    $q = mysqli_query($con, "INSERT INTO usuarios (nombre, telefono, email, ApellidoP, ApellidoM, contrasena) VALUES ('$nombre', '$telefono', '$email', '$APaterno', '$APaterno', '$pass')");
 
     if ($q){
         http_response_code(201);
